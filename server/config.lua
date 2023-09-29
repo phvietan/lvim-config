d51f78802350dc6a328c3e296872b318147e56d9
An Pham <phvietan@gmail.com>
2023-09-29 19:46:47 +0700

    Update config.lua

--- server/config.lua
+++ server/config.lua
@@ -6,14 +6,25 @@ vim.opt.clipboard = 'unnamedplus'
 vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
 vim.opt.mouse = 'a'
 
+
+vim.filetype.add({
+    extension = {
+        ejs = "html",
+    },
+})
+
 lvim.plugins = {
   {"akinsho/toggleterm.nvim", version = '*', config = function()
   require("toggleterm").setup({
       size = 20,
     })
 end},
-
 }
+
+local function open_nvim_tree()
+  require("nvim-tree.api").tree.open()
+end
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
 function ForceKillBuffer()
   local kill_cmd = "bd"
@@ -21,17 +32,14 @@ function ForceKillBuffer()
   require("lvim.core.bufferline").buf_kill(kill_cmd, 0, force)
 end
 
-lvim.keys.normal_mode["<leader>x"] = ":lua ForceKillBuffer()<cr>"
+lvim.keys.normal_mode["<C-w>"] = ":lua ForceKillBuffer()<cr>"
 lvim.keys.normal_mode["<Tab>"] = ":bnext<cr>"
 lvim.keys.normal_mode["<S-Tab>"] = ":bprev<cr>"
 lvim.keys.normal_mode["|"] = ":vsplit<CR>"
-lvim.keys.normal_mode["-"] = ":split<CR>"
-lvim.keys.normal_mode["<C-Right>"] = ":BufferLineCycleNext<CR>"
-lvim.keys.normal_mode["<C-Left>"] = ":BufferLineCyclePrev<CR>"
+lvim.keys.normal_mode["-"] = ":hsplit<CR>"
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 
 lvim.builtin.terminal.execs = {
-      { vim.o.shell, "<leader>`", "Horizontal Terminal", "horizontal", 0.4 },
+  { vim.o.shell, "<leader>`", "Horizontal Terminal", "horizontal", 0.4 },
+  { vim.o.shell, "<C-`>", "Horizontal Terminal", "horizontal", 0.4 },
 }
-
--- vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
-
